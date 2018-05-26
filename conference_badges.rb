d69851f2991d826d367468643084f3f6ff911358
Matt Farmer <mrfarmer777@gmail.com>
2018-05-26 04:32:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}"
 end
+
+def batch_badge_creator(name_array)
+  result=Array.new
+  name_array.each do |name|
+    result.push(badge_maker(name))
+  end
+  return result
+end
