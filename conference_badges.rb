adaf8b674db609d8494416f626cb6221dffaa352
Matt Farmer <mrfarmer777@gmail.com>
2018-05-26 04:39:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,4 +16,14 @@ def assign_rooms(name_array)
   name_array.each_with_index {|name,index| result.push("Hello, #{name}! You'll be assigned to room #{index+1}!")}
   return result
 end
-  
+
+def printer(name_array)
+  rooms = assign_rooms(name_array)
+  badges = batch_badge_creator(name_array)
+  badges.each do |greeting|
+    puts greeting
+  end
+  rooms.each do |assignment|
+    puts assignment
+  end
+end
